185072f98f027ab810bd599ba406d85c48b994ed
vikas_dangi <vikasdangi8moon@gmail.com>
2022-08-09 01:45:22 +0530

    Revert "added styles for form posts and app"
    This reverts commit 440f8e3f037169226d49cee531205b227fe898f0.

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,14 @@ import React from 'react';
 
 import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 
-import Posts from './Components/Posts/posts.js';
-import Form from './Components/Form/form.js';
-import memories from './images/abcd.png';
-import useStyles from './styles';
+import memories from './images/abcd.png'
 
 const App= () => {
-    const classes=useStyles();
-
     return (
         <Container maxwidth="lg">
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-                <img className={classes.image} src={memories} alt="memories" height="60" />
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Memories</Typography>
+                <img src={memories} alt="memories" height="60" />
             </AppBar>
             <Grow in>
                 <Container>
@@ -22,7 +17,7 @@ const App= () => {
                         <Grid item xs={12} sm={7}>
                             <Posts />
                         </Grid>
-                        <Grid item xs={12} sm={4}>
+                        <Grid item xs={12} sm={7}>
                             <Form />
                         </Grid>
                     </Grid>
